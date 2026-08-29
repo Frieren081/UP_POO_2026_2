82c3eed576e512b87046eaf4598fcd680aac835d
Frieren081 <29109298Br34@>
2026-08-29 17:31:37 -0300

    IMC_calc-v2

--- Aula3/src/main/java/Aula.java
+++ Aula3/src/main/java/Aula.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
+
 public class Aula {
         static void main() {
 
+                String nome = "";
+                double altura = 0;
+                double peso = 0;
 
-        double altura = 1.70;
-        double peso = 60;
-        double IMC = peso/(altura*altura);
-        if(IMC ==  )
-                System.out.println("");
+                Scanner scanner = new Scanner(System.in);
+                System.out.printf("Digite seu nome: ");
+                nome = scanner.nextLine();
+                System.out.printf("Digite sua altura: ");
+                altura = scanner.nextDouble();
+                System.out.printf("Digite seu peso: ");
+                peso = scanner.nextDouble();
 
+                double IMC = peso / (altura * altura);
+
+                if (IMC < 18.5) {
+                        System.out.println(nome + ", você está abaixo do peso!");
+                        System.out.println("IMC respectivo: " + IMC);
+
+                } else if (IMC == 18.5) {
+                        System.out.println(nome + ", você está no peso adequado!");
+                        System.out.println("IMC respectivo: " + IMC);
+
+                } else if (IMC <= 24.9) {
+                        System.out.println(nome + ", você está acima do peso!");
+                        System.out.println("IMC respectivo: " + IMC);
+
+                } else if (IMC >= 25.00) {
+                        System.out.println(nome + ", você entrou no grau de obesidade, procure ajuda médica imediatamente!");
+                        System.out.println("IMC respectivo: " + IMC);
+
+                }
         }
 }
